38addd18c0e89903a32ffe473238892e77169bb9
Ana Araújo <araujo.ana92@gmail.com>
2024-03-31 15:46:09 +0100

    update

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,33 +1,38 @@
-""" Function for clean data """
 import argparse
 import pandas as pd
 
-def clean_data(country="PT"):
-    """Function to load and clean the tsv file for a specific country.
+def load_data(path):
+    """Load data from a TSV file."""
+    df = pd.read_csv(path, sep="\t")
+    df.columns = [col.replace("\\", "") for col in df.columns]
+    return df
 
-    Returns dataframe cleaned and saved as csv
-    """
-    df = pd.read_csv("life_expectancy/data/eu_life_expectancy_raw.tsv", sep="\t")
-    df.columns =  [col.replace("\\","") for col in df.columns]
-
+def clean_data(df, country="PT"):
+    """Clean the data to a specific country."""
     df_melt = df.melt(id_vars=["unit,sex,age,geotime"],
-                                   var_name="year", value_name="value")
-
-    df_melt[['unit', 'sex','age', 'region'
+                      var_name="year", value_name="value")
+    df_melt[['unit', 'sex', 'age', 'region'
              ]] = df_melt['unit,sex,age,geotime'].str.split(",", expand=True)
-
     df_melt.drop(columns=['unit,sex,age,geotime'], inplace=True)
     df_melt['year'] = df_melt['year'].str.extract(r'(\d+)').astype(int)
     df_melt['value'] = pd.to_numeric(df_melt['value'], errors='coerce')
     df_melt = df_melt.dropna(subset=['value'])
     c_life_expec = df_melt[df_melt['region'] == country]
-    # adjust index and columns
-    c_life_expec.columns = ['unit', 'sex', 'age', 'region', 'year', 'value']
-    c_life_expec = c_life_expec[['unit', 'sex', 'age', 'region', 'year','value']]
-    c_life_expec.to_csv("life_expectancy/data/pt_life_expectancy.csv", index=False)
     return c_life_expec
-if __name__ == "__main__":  # pragma: no cover
+
+def save_data(df, path):
+    """Save the data to a CSV file."""
+    df.to_csv(path, index=False)
+
+def main(path="life_expectancy/data/eu_life_expectancy_raw.tsv", country="PT"):
+    """Main function to load, clean, and save data."""
+    df = load_data(path)
+    cleaned_data = clean_data(df, country)
+    save_data(cleaned_data, "life_expectancy/data/pt_life_expectancy.csv")
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--path', default='life_expectancy/data/eu_life_expectancy_raw.tsv')
     parser.add_argument('--country', default='PT')
     args = parser.parse_args()
-    clean_data(args.country)
+    main(args.path, args.country)
