e0a5f33d6f5010a3edc3ffdff678f56a7faf33f9
Ana Araújo <araujo.ana92@gmail.com>
2024-05-14 08:33:52 +0100

    modified:   life_expectancy/tests/new_fixtures.py

--- life_expectancy/tests/new_fixtures.py
+++ life_expectancy/tests/new_fixtures.py
@@ -8,12 +8,12 @@ FIXTURES_DIR = Path("C:/Users/afaraujo/OneDrive - Hovione/Documents/DareData/Fou
 if __name__ == "__main__":
     original_data = pd.read_csv(DATA_DIR /
                                 "eu_life_expectancy_raw.tsv", sep="\t")
-    generated_input = original_data.sample(n=100, random_state=0)
-    generated_input.to_csv(FIXTURES_DIR /
+    sample_input = original_data.sample(n=100, random_state=0)
+    sample_input.to_csv(FIXTURES_DIR /
                            "eu_life_expectancy_raw_sample.tsv",
                             index=False, sep="\t")
 
-    generated_output = clean_data(generated_input)
-    generated_output.to_csv(FIXTURES_DIR /
+    sample_output = clean_data(sample_input)
+    sample_output.to_csv(FIXTURES_DIR /
                             "eu_life_expectancy_raw_sample_expected.csv",
                             index=False)
