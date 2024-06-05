0a97e90919aee11e555c96ff3d18779a98865b95
Ana Araújo <araujo.ana92@gmail.com>
2024-06-05 15:55:44 +0100

    modified:   life_expectancy/loadings_savings.py modified:   life_expectancy/tests/new_fixtures.py modified:   life_expectancy/tests/test_cleaning.py

--- life_expectancy/tests/new_fixtures.py
+++ life_expectancy/tests/new_fixtures.py
@@ -1,19 +1,28 @@
 from pathlib import Path
 import pandas as pd
 from life_expectancy.cleaning import clean_data
+from life_expectancy.regions import Region
+import argparse
 
-DATA_DIR = Path("C:/Users/afaraujo/OneDrive - Hovione/Documents/DareData/Foundations-LP/assignments/life_expectancy/data")
-FIXTURES_DIR = Path("C:/Users/afaraujo/OneDrive - Hovione/Documents/DareData/Foundations-LP/assignments/life_expectancy/tests/fixtures")
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "life_expectancy/data"
+FIXTURES_DIR = BASE_DIR / "life_expectancy/tests/fixtures"
+
+def create_fixtures(country: Region) -> None:
+    """Create fixtures for testing.
+    Args: country (Region): The country code to filter the data."""
+    original_data = pd.read_csv(DATA_DIR / "eu_life_expectancy_raw.tsv", sep="\t")
+    sample_input = original_data.sample(n=100, random_state=0)
+    sample_input.to_csv(FIXTURES_DIR / "eu_life_expectancy_raw_sample.tsv", index=False, sep="\t")
+
+    sample_output = clean_data(sample_input, country)
+    sample_output.to_csv(FIXTURES_DIR / "eu_life_expectancy_raw_sample_expected.csv", index=False)
 
 if __name__ == "__main__":
-    original_data = pd.read_csv(DATA_DIR /
-                                "eu_life_expectancy_raw.tsv", sep="\t")
-    sample_input = original_data.sample(n=100, random_state=0)
-    sample_input.to_csv(FIXTURES_DIR /
-                           "eu_life_expectancy_raw_sample.tsv",
-                            index=False, sep="\t")
-
-    sample_output = clean_data(sample_input)
-    sample_output.to_csv(FIXTURES_DIR /
-                            "eu_life_expectancy_raw_sample_expected.csv",
-                            index=False)
+    parser = argparse.ArgumentParser(description="Create fixtures for life expectancy data.")
+    parser.add_argument('--country', type=str, required=True, help='Country code to filter the data.')
+    args = parser.parse_args()
+    
+    country = Region[args.country]
+    
